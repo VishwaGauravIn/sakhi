4b4cdac88486d788b196c9839959ad543885aa15
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2024-01-17 17:38:19 +0530

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>
         <HeaderNav />
         {children}
