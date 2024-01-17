87fe518bd21e300f337f8606255cd3921ad5319c
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2024-01-17 14:32:38 +0530

    new icons + navbar optimizations

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,73 +1,56 @@
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
-import { FiHome } from "react-icons/fi";
+import { FiHome, FiCalendar, FiBookOpen } from "react-icons/fi";
+import { HiOutlineUserGroup } from "react-icons/hi2";
 
 export default function NavBar() {
   const [active, setActive] = useState(0);
+  const router = useRouter();
+
+  const handleClick = (id) => {
+    setActive(id);
+    // router.push(id);
+  };
+
   return (
-    <div className="absolute w-full bottom-0 h-20 ring flex gap-1 overflow-hidden">
-      <div
-        className="flex-1 border-t-2 flex justify-center items-center relative"
-        onClick={() => setActive(0)}
-      >
-        <FiHome
-          className={`h-8 w-8 transition-all ease-in-out ${
-            active === 0 && "stroke-pink-400 scale-110"
-          } `}
+    <div className="absolute w-full bottom-0 h-20 flex gap-1 overflow-hidden">
+      {navItems.map((item) => (
+        <NavItem
+          key={item.id}
+          id={item.id}
+          icon={item.icon}
+          label={item.label}
+          active={active}
+          onClick={handleClick}
         />
-        <span
-          className={`absolute bottom-0 h-8 w-full bg-pink-400 rounded-t-full -mb-2 transition-all ease-in-out duration-300 ${
-            active === 0 ? "-mb-6 opacity-100" : "-mb-8 opacity-0"
-          }`}
-        />
-      </div>
-
-      <div
-        className="flex-1 border-t-2 flex justify-center items-center relative"
-        onClick={() => setActive(1)}
-      >
-        <FiHome
-          className={`h-8 w-8 transition-all ease-in-out ${
-            active === 1 && "stroke-pink-400 scale-110"
-          } `}
-        />
-        <span
-          className={`absolute bottom-0 h-8 w-full bg-pink-400 rounded-t-full -mb-2 transition-all ease-in-out duration-300 ${
-            active === 1 ? "-mb-6 opacity-100" : "-mb-8 opacity-0"
-          }`}
-        />
-      </div>
-
-      <div
-        className="flex-1 border-t-2 flex justify-center items-center relative"
-        onClick={() => setActive(2)}
-      >
-        <FiHome
-          className={`h-8 w-8 transition-all ease-in-out ${
-            active === 2 && "stroke-pink-400 scale-110"
-          } `}
-        />
-        <span
-          className={`absolute bottom-0 h-8 w-full bg-pink-400 rounded-t-full -mb-2 transition-all ease-in-out duration-300 ${
-            active === 2 ? "-mb-6 opacity-100" : "-mb-8 opacity-0"
-          }`}
-        />
-      </div>
-
-      <div
-        className="flex-1 border-t-2 flex justify-center items-center relative"
-        onClick={() => setActive(3)}
-      >
-        <FiHome
-          className={`h-8 w-8 transition-all ease-in-out ${
-            active === 3 && "stroke-pink-400 scale-110"
-          } `}
-        />
-        <span
-          className={`absolute bottom-0 h-8 w-full bg-pink-400 rounded-t-full -mb-2 transition-all ease-in-out duration-300 ${
-            active === 3 ? "-mb-6 opacity-100" : "-mb-8 opacity-0"
-          }`}
-        />
-      </div>
+      ))}
     </div>
   );
 }
+
+const navItems = [
+  { id: 0, icon: <FiHome />, label: "Dashboard" },
+  { id: 1, icon: <FiCalendar />, label: "Period Tracker" },
+  { id: 2, icon: <FiBookOpen />, label: "Jornal" },
+  { id: 3, icon: <HiOutlineUserGroup />, label: "Social" },
+];
+
+const NavItem = ({ id, icon, label, active, onClick }) => (
+  <div
+    className={`flex-1 border-t-2 flex justify-center items-center relative transition-all ease-in-out ${
+      active === id && "border-pink-400"
+    } `}
+    onClick={() => onClick(id)}
+  >
+    {React.cloneElement(icon, {
+      className: `h-8 w-8 transition-all ease-in-out ${
+        active === id ? "stroke-pink-400 scale-110" : "stroke-slate-700"
+      }`,
+    })}
+    <span
+      className={`absolute bottom-0 h-8 w-full bg-pink-400 rounded-t-full -mb-2 transition-all ease-in-out duration-300 ${
+        active === id ? "-mb-6 opacity-100" : "-mb-8 opacity-0"
+      }`}
+    />
+  </div>
+);
