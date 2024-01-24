80d6f4f2b4538d75d0fe088b88c632af392b3d67
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2024-01-25 01:09:54 +0530

    Add user name to journal page and add buttons for adding content

--- src/app/(protected)/journal/page.js
+++ src/app/(protected)/journal/page.js
@@ -1,7 +1,25 @@
-import React from 'react'
+'use client';
+
+import React from 'react';
+import { useSession } from 'next-auth/react';
+import { HiOutlineMicrophone, HiPlus } from 'react-icons/hi2';
 
 export default function Journal() {
+
+  const { data } = useSession();
+  const userFirstName = data?.user?.name ? data.user.name.split(" ")[0] : "";
+
   return (
-    <div>Journal</div>
-  )
+    <div>
+      <div className='text-base p-4 font-semibold'>
+        Hello {userFirstName}! <br />
+        <span className='text-[#389F8A]'>Write down what&apos;s on your mind today</span> 
+      </div>
+
+      <div className='fixed bottom-20 left-1/2 -translate-x-1/2 z-10 p-2 rounded-full flex flex-row justify-center items-center gap-2 bg-white ring-1 ring-gray-200'>
+      <button className='h-14 aspect-square flex justify-center items-center rounded-full bg-black text-pink-50'><HiPlus size={32} /></button>
+      <button className='h-14 aspect-square flex justify-center items-center rounded-full bg-black/5 text-pink-950'><HiOutlineMicrophone size={32} /></button>
+    </div>
+    </div>
+  );
 }
