b5c9f6c62d0209de7e62ce7d5b97d82e9903b3b6
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2024-01-24 16:00:38 +0530

    manifest improvements

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,9 +11,6 @@ export const metadata = {
   description:
     "Sakhi, a mobile-first app tailored for women, encompasses daily journals, safety features, community, and holistic health tools. Elevate your well-being with Sakhi, your dedicated companion for empowerment, connection, and growth.",
   lang: "en",
-  manifest: "/manifest.json",
-  viewport:
-    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
   icons: [
     { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
     { rel: "icon", url: "icons/icon-128x128.png" },
@@ -25,6 +22,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
       </head>
       <body className={exo2.className}>
         <HeaderNav />
