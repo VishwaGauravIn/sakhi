cb08edda5b01c454ec004db3cf0424e14393892c
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2024-01-24 14:54:28 +0530

    Dashboard Design with Super-Refactoring

--- src/app/(protected)/dashboard/page.js
+++ src/app/(protected)/dashboard/page.js
@@ -4,11 +4,51 @@ import Greeting from "@/components/dashboard/Greeting";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import React from "react";
-import { Bs0Circle } from "react-icons/bs";
+import { FaBookOpen } from "react-icons/fa6";
+import { MdEmergency, MdWaterDrop } from "react-icons/md";
+import { AiFillSafetyCertificate } from "react-icons/ai";
+import { RiMentalHealthFill } from "react-icons/ri";
+import { HiChatBubbleBottomCenterText, HiUserGroup } from "react-icons/hi2";
+import { BiSolidAlarmExclamation } from "react-icons/bi";
+import { GiCupcake } from "react-icons/gi";
+import Link from "next/link";
 
 export default function Dashboard() {
   const { data } = useSession();
   const userFirstName = data?.user?.name ? data.user.name.split(" ")[0] : "";
+
+  const menuItems = [
+    { color: "rose", label: "Cycle", href: "/tracker", icon: MdWaterDrop },
+    { color: "amber", label: "Journal", href: "/journal", icon: FaBookOpen },
+    {
+      color: "green",
+      label: "Safety",
+      href: "/",
+      icon: AiFillSafetyCertificate,
+    },
+    { color: "fuchsia", label: "Support", href: "/", icon: RiMentalHealthFill },
+    { color: "orange", label: "Community", href: "/social", icon: HiUserGroup },
+    {
+      color: "sky",
+      label: "Sakhi Bot",
+      href: "/",
+      icon: HiChatBubbleBottomCenterText,
+    },
+    {
+      color: "emerald",
+      label: "Reminders",
+      href: "/",
+      icon: BiSolidAlarmExclamation,
+    },
+    { color: "pink", label: "Emergency", href: "/", icon: MdEmergency },
+    {
+      color: "cyan",
+      label: "Desserts",
+      href: "https://www.swiggy.com/desserts-restaurants-near-me",
+      icon: GiCupcake,
+    },
+  ];
+
   return (
     <div>
       <Greeting userFirstName={userFirstName} />
@@ -30,35 +70,27 @@ export default function Dashboard() {
       </div>
       {/* Grid Menu */}
       <div className="grid grid-cols-3 gap-4">
-        <div className="w-full aspect-square rounded-md flex flex-col justify-center items-center shadow-xl bg-gradient-to-br from-rose-200 to-rose-300">
-          <Bs0Circle className="w-1/2 h-1/2 fill-rose-900" />
-        </div>
-        <div className="w-full aspect-square rounded-md flex flex-col justify-center items-center shadow-xl bg-gradient-to-br from-amber-200 to-amber-300">
-          <Bs0Circle className="w-1/2 h-1/2 fill-amber-900" />
-        </div>
-        <div className="w-full aspect-square rounded-md flex flex-col justify-center items-center shadow-xl bg-gradient-to-br from-green-200 to-green-300">
-          <Bs0Circle className="w-1/2 h-1/2 fill-green-900" />
-        </div>
-        <div className="w-full aspect-square rounded-md flex flex-col justify-center items-center shadow-xl bg-gradient-to-br from-fuchsia-200 to-fuchsia-300">
-          <Bs0Circle className="w-1/2 h-1/2 fill-fuchsia-900" />
-        </div>
-        <div className="w-full aspect-square rounded-md flex flex-col justify-center items-center shadow-xl bg-gradient-to-br from-orange-200 to-orange-300">
-          <Bs0Circle className="w-1/2 h-1/2 fill-orange-900" />
-        </div>
-        <div className="w-full aspect-square rounded-md flex flex-col justify-center items-center shadow-xl bg-gradient-to-br from-sky-200 to-sky-300">
-          <Bs0Circle className="w-1/2 h-1/2 fill-sky-900" />
-        </div>
-        <div className="w-full aspect-square rounded-md flex flex-col justify-center items-center shadow-xl bg-gradient-to-br from-emerald-200 to-emerald-300">
-          <Bs0Circle className="w-1/2 h-1/2 fill-emerald-900" />
-        </div>
-        <div className="w-full aspect-square rounded-md flex flex-col justify-center items-center shadow-xl bg-gradient-to-br from-pink-200 to-pink-300">
-          <Bs0Circle className="w-1/2 h-1/2 fill-pink-900" />
-        </div>
-        <div className="w-full aspect-square rounded-md flex flex-col justify-center items-center shadow-xl bg-gradient-to-br from-cyan-200 to-cyan-300">
-          <Bs0Circle className="w-1/2 h-1/2 fill-cyan-900" />
-        </div>
+        {menuItems.map((item) => (
+          <GridElement key={item.label} data={item} />
+        ))}
       </div>
       dashboard
     </div>
+  );
+}
+
+function GridElement({ key, data }) {
+  return (
+    <Link href={data.href}>
+      <div
+        key={key}
+        className={`w-full aspect-square rounded-md flex flex-col justify-center items-center shadow-lg gap-1 bg-gradient-to-br from-${data.color}-100 to-${data.color}-200 text-${data.color}-950`}
+      >
+        {React.createElement(data.icon, {
+          className: `w-1/2 h-1/3 fill-${data.color}-900`,
+        })}
+        <span className="font-semibold">{data.label}</span>
+      </div>
+    </Link>
   );
 }
