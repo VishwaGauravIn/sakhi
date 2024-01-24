36ea250f1f31a5de308eb5a0fa750c8967f82591
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2024-01-24 12:43:29 +0530

    wwyltdt hero

--- src/app/(protected)/dashboard/page.js
+++ src/app/(protected)/dashboard/page.js
@@ -1,5 +1,31 @@
+"use client";
+
+import Greeting from "@/components/dashboard/Greeting";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 import React from "react";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const { data } = useSession();
+  const userFirstName = data?.user?.name ? data.user.name.split(" ")[0] : "";
+  return (
+    <div>
+      <Greeting userFirstName={userFirstName} />
+      <hr className="w-full" />
+      <div className="flex my-4">
+        <div className="w-6/12 text-center flex flex-col justify-center items-center text-lg font-bold translate-x-8 text-[#DB6542]">
+          What Would You Like<span className="text-[#389F8A]">to do Today?</span>
+        </div>
+        <div className="w-6/12 overflow-hidden">
+          <Image
+            src={"/assets/dashboard/think.png"}
+            width={998}
+            height={622}
+            className="translate-x-10"
+          />
+        </div>
+      </div>
+      Dashboard
+    </div>
+  );
 }
