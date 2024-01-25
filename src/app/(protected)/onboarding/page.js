5316908462b7ffad78f9466fed5b72d6aebafd3e
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2024-01-25 23:41:44 +0530

    onboarding page

--- src/app/(protected)/onboarding/page.js
+++ src/app/(protected)/onboarding/page.js
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { useSession } from "next-auth/react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BsChevronDoubleRight } from "react-icons/bs";
 
 // TODO: create a form asking for required infos and send the data on submit, redirect to dashboard
@@ -12,6 +12,21 @@ export default function Onboarding() {
   const userFirstName = data?.user?.name ? data.user.name.split(" ")[0] : "";
 
   const [initiated, setInitiated] = useState(false);
+
+  const [menstrualData, setMenstrualData] = useState({
+    last_period_start_date: "",
+    average_cycle_length: 22,
+    variations_in_cycle_length: 0,
+    period_duration: 5,
+    recent_changes_in_your_cycle: "",
+    underlying_health_conditions: "",
+    medications: "",
+    stress: "",
+  });
+
+  useEffect(() => {
+    console.log(menstrualData);
+  }, [menstrualData]);
   return (
     <div className="flex flex-col gap-4">
       <p className="w-full text-center text-2xl text-pink-400 mt-10">
@@ -40,6 +55,171 @@ export default function Onboarding() {
           <p className="text-sm opacity-90">
             Please fill these info so that we can personalize our app for you:
           </p>
+          <form
+            onSubmit={() => alert(55)}
+            className="flex flex-col gap-4 accent-pink-400"
+          >
+            <label htmlFor="name" className="text-sm">
+              Last period start date*
+            </label>
+            <input
+              type="date"
+              name="name"
+              id="name"
+              required
+              value={menstrualData.last_period_start_date}
+              onChange={(e) =>
+                setMenstrualData({
+                  ...menstrualData,
+                  last_period_start_date: e.target.value,
+                })
+              }
+              className="w-full p-2 border-2 border-pink-400 rounded-md"
+            />
+            <label htmlFor="name" className="text-sm">
+              Average cycle length* [21-35 days normally]
+            </label>
+            <div className="flex">
+              <input
+                type="range"
+                name=""
+                id=""
+                min={14}
+                max={40}
+                required
+                defaultValue={22}
+                className="w-11/12"
+                value={menstrualData.average_cycle_length}
+                onChange={(e) =>
+                  setMenstrualData({
+                    ...menstrualData,
+                    average_cycle_length: e.target.value,
+                  })
+                }
+              />
+              <span className="mx-auto">
+                {menstrualData.average_cycle_length}
+              </span>
+            </div>
+            <label htmlFor="name" className="text-sm">
+              Variations in cycle length* [+/- 7 days normally]
+            </label>
+            <div className="flex">
+              <input
+                type="range"
+                name=""
+                id=""
+                min={0}
+                max={7}
+                required
+                defaultValue={0}
+                className="w-11/12"
+                value={menstrualData.variations_in_cycle_length}
+                onChange={(e) =>
+                  setMenstrualData({
+                    ...menstrualData,
+                    variations_in_cycle_length: e.target.value,
+                  })
+                }
+              />
+              <span className="mx-auto">
+                {menstrualData.variations_in_cycle_length}
+              </span>
+            </div>
+            <label htmlFor="name" className="text-sm">
+              Period duration* [3-7 days normally]
+            </label>
+            <div className="flex">
+              <input
+                type="range"
+                name=""
+                id=""
+                required
+                min={0}
+                max={7}
+                defaultValue={3}
+                className="w-11/12"
+                value={menstrualData.period_duration}
+                onChange={(e) =>
+                  setMenstrualData({
+                    ...menstrualData,
+                    period_duration: e.target.value,
+                  })
+                }
+              />
+              <span className="mx-auto">{menstrualData.period_duration}</span>
+            </div>
+            <label htmlFor="name" className="text-sm">
+              Recent Changes In Your Cycle
+            </label>
+            <input
+              type="text"
+              name="name"
+              placeholder="No recent changes"
+              value={menstrualData.recent_changes_in_your_cycle}
+              onChange={(e) =>
+                setMenstrualData({
+                  ...menstrualData,
+                  recent_changes_in_your_cycle: e.target.value,
+                })
+              }
+              className="w-full p-2 border-2 border-pink-400 rounded-md"
+            />
+            <label htmlFor="name" className="text-sm">
+              Underlying Health Conditions
+            </label>
+            <input
+              type="text"
+              name="name"
+              placeholder="eg. PCOS, Endometriosis, Thyroid, etc."
+              value={menstrualData.underlying_health_conditions}
+              onChange={(e) =>
+                setMenstrualData({
+                  ...menstrualData,
+                  underlying_health_conditions: e.target.value,
+                })
+              }
+              className="w-full p-2 border-2 border-pink-400 rounded-md"
+            />
+            <label htmlFor="name" className="text-sm">
+              Medications
+            </label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Birth control pills"
+              value={menstrualData.medications}
+              onChange={(e) =>
+                setMenstrualData({
+                  ...menstrualData,
+                  medications: e.target.value,
+                })
+              }
+              className="w-full p-2 border-2 border-pink-400 rounded-md"
+            />
+            <label htmlFor="name" className="text-sm">
+              Stress
+            </label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Low, Medium, High"
+              value={menstrualData.stress}
+              onChange={(e) =>
+                setMenstrualData({
+                  ...menstrualData,
+                  stress: e.target.value,
+                })
+              }
+              className="w-full p-2 border-2 border-pink-400 rounded-md"
+            />
+            <Button
+              type="submit"
+              className="self-center flex text-xl py-3 px-6  bg-gradient-to-r from-pink-400 to-pink-500 shadow-lg shadow-pink-500/20 ring-1 ring-pink-200"
+            >
+              Submit
+            </Button>
+          </form>
         </>
       )}
     </div>
--- src/app/(protected)/tracker/page.js
+++ src/app/(protected)/tracker/page.js
@@ -3,3 +3,4 @@ import React from "react";
 export default function Tracker() {
   return <div>Tracker</div>;
 }
+
