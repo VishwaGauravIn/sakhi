475ee36c0997d1c9cd8d59567de557142bac259f
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2024-01-25 23:39:00 +0530

    Add Greeting component to Journal page

--- src/app/(protected)/journal/page.js
+++ src/app/(protected)/journal/page.js
@@ -7,6 +7,7 @@ import Image from 'next/image';
 
 import { BiAngry, BiHappyHeartEyes, BiSad, BiSmile } from 'react-icons/bi';
 import Link from 'next/link';
+import Greeting from '@/components/dashboard/Greeting';
 
 export default function Journal() {
   const { data } = useSession();
@@ -75,54 +76,61 @@ export default function Journal() {
 
   return (
     <div className='h-[calc(100vh-140px)]'>
-      <div className='text-base p-4 font-semibold'>
+      <Greeting userFirstName={userFirstName} />
+      <div className='flex flex-row p-4'>
+        <div className='w-2/3'>
+          <div className='text-base font-semibold'>
+         
+              Write down what&apos;s on your mind today
+       
+          </div>
+          <button className='bg-gradient-to-tr from-emerald-300 to-emerald-200 text-emerald-950 shadow-sm shadow-gray-200 py-2 px-4 mt-2 rounded-lg'>
+            Daily Check in
+          </button>
+        </div>
         <Image
           src='/assets/journal/journal.jpg'
           width={500}
           height={500}
-          className='self-center'
+          className='self-center w-1/3'
         />
-        Hello {userFirstName}! <br />
-        <span className='text-[#389F8A]'>
-          Write down what&apos;s on your mind today
-        </span>
       </div>
       <div className='p-4'>
-          <div className='text-base pb-4 font-semibold'>Recent Notes</div>
+        <div className='text-base pb-4 font-semibold'>Recent Notes</div>
 
-          {notes.map((note, index) => (
-            <Link href={'#'} key={index}>
-              <div
-                className={`w-full rounded-md flex flex-row mb-2 justify-between items-start p-2 shadow-md gap-1 bg-gradient-to-br from-${getMoodBasedColor(
-                  note.mood
-                )}-100 to-${getMoodBasedColor(
-                  note.mood
-                )}-200 text-${getMoodBasedColor(note.mood)}-950`}
-              >
-                <div className='max-w-[80%] flex flex-row justify-start items-center'>
-                  <div className='font-semibold text-center'>
-                    {getDateAndMonthFromDate(note.date)}
-                  </div>{' '}
-                  <div
-                    className={`mx-2 h-10 w-[2px] rounded-[1px] bg-${getMoodBasedColor(
-                      note.mood
-                    )}-900`}
-                  ></div>{' '}
-                  {note.text}
-                </div>
-
-                {React.createElement(getMoodBasedEmoji(note.mood), {
-                  size: 24,
-                  className: `fill-${getMoodBasedColor(note.mood)}-900`,
-                })}
+        {notes.map((note, index) => (
+          <Link href={'#'} key={index}>
+            <div
+              className={`w-full rounded-md flex flex-row mb-2 justify-between items-start p-2 shadow-md gap-1 bg-gradient-to-br from-${getMoodBasedColor(
+                note.mood
+              )}-100 to-${getMoodBasedColor(
+                note.mood
+              )}-200 text-${getMoodBasedColor(note.mood)}-950`}
+            >
+              <div className='max-w-[80%] flex flex-row justify-start items-center'>
+                <div className='font-semibold text-center px-2'>
+                  {getDateAndMonthFromDate(note.date)}
+                </div>{' '}
+                <div
+                  className={`ml-2 mr-4 h-10 w-[2px] rounded-[1px] bg-${getMoodBasedColor(
+                    note.mood
+                  )}-900`}
+                ></div>{' '}
+                {note.text}
               </div>
-            </Link>
-          ))}
+
+              {React.createElement(getMoodBasedEmoji(note.mood), {
+                size: 36,
+                className: `fill-${getMoodBasedColor(note.mood)}-900`,
+              })}
+            </div>
+          </Link>
+        ))}
       </div>
 
-     <div className='h-36'>
-      {/* This div is only for page to get a full scroll */}
-     </div>
+      <div className='h-36'>
+        {/* This div is only for page to get a full scroll */}
+      </div>
 
       <div className='fixed bottom-20 left-1/2 -translate-x-1/2 z-10 p-2 rounded-full flex flex-row justify-center items-center gap-2 bg-white ring-1 ring-gray-200'>
         <button className='h-12 aspect-square flex justify-center items-center rounded-full bg-black text-pink-50'>
