930c3ac24b091f76a7cf332ee26aa7c636dfaf30
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2024-02-04 22:40:17 +0530

    Add location permission check and display alert message

--- src/app/(protected)/safety/page.js
+++ src/app/(protected)/safety/page.js
@@ -1,14 +1,20 @@
 'use client';
+import Greeting from '@/components/dashboard/Greeting';
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import { BsHospital } from 'react-icons/bs';
 import { FaLocationArrow } from 'react-icons/fa';
 import { GrUserPolice } from 'react-icons/gr';
 import { IoIosArrowForward } from 'react-icons/io';
-import { MdEmergency } from 'react-icons/md';
-import { GoAlertFill } from 'react-icons/go';
+import { MdEmergency, MdSportsMartialArts } from 'react-icons/md';
+import { TbAlertHexagon } from 'react-icons/tb';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 
 const Safety = () => {
+  const { data } = useSession();
+  const userFirstName = data?.user?.name ? data.user.name.split(' ')[0] : '';
+
   const [isLocationPermissionGranted, setIsLocationPermissionGranted] = useState(false);
 
   const getCurrentUserLocation = () => {
@@ -42,21 +48,27 @@ const Safety = () => {
   const openNearby = (x) => {
     window.open(
       `https://www.google.com/maps/search/${x}/@${localStorage.getItem(
-        "sakhi_lat"
-      )},${localStorage.getItem("sakhi_long")}`,
-      "_blank"
+        'sakhi_lat'
+      )},${localStorage.getItem('sakhi_long')}`,
+      '_blank'
     );
-  }
+  };
 
   return (
     <div>
-      {!isLocationPermissionGranted &&
-        'Please allow location permission to use this feature.'}
-
-        <p className='my-2'>Here are some nearby resources to help you out</p>
-        <div className='border shadow-sm border-gray-200 rounded-xl'>
+      {!isLocationPermissionGranted && (
+        <Alert>
+          <TbAlertHexagon className='h-4 w-4' />
+          <AlertTitle>
+            Please allow location permission to use these features.
+          </AlertTitle>
+        </Alert>
+      )}
+      <Greeting userFirstName={userFirstName} />
+      <p className='my-2'>Here are some nearby resources to help you out</p>
+      <div className='border shadow-sm border-gray-200 rounded-xl'>
         <button
-          onClick={() => openNearby("hospitals")}
+          onClick={() => openNearby('hospitals')}
           className='cursor-pointer p-3 text-center w-full flex flex-row justify-between items-center gap-2 rounded-xl'
         >
           <div className='flex flex-row justify-start items-center gap-2 '>
@@ -67,16 +79,16 @@ const Safety = () => {
         </button>
         <hr />
         <button
-          onClick={() => openNearby("police station")}
+          onClick={() => openNearby('police station')}
           className='cursor-pointer p-3 text-center w-full flex flex-row justify-between items-center gap-2 rounded-xl'
         >
           <div className='flex flex-row justify-start items-center gap-2 '>
             <GrUserPolice className='text-lg text-emerald-600' />
-            Police Station
+            Police Stations
           </div>
           <FaLocationArrow className='text-lg' />
         </button>
-        
+
         <hr />
         <Link
           href={'/emergency'}
@@ -88,7 +100,18 @@ const Safety = () => {
           </div>
           <IoIosArrowForward className='text-lg' />
         </Link>
-       </div>
+        <hr />
+        <Link
+          href={'/martial-art-for-safety'}
+          className='cursor-pointer p-3 text-center w-full flex flex-row justify-between items-center gap-2 rounded-xl'
+        >
+          <div className='flex flex-row justify-start items-center gap-2 '>
+            <MdSportsMartialArts className='text-lg text-yellow-600' />
+            Self Defence Tips
+          </div>
+          <IoIosArrowForward className='text-lg' />
+        </Link>
+      </div>
     </div>
   );
 };
