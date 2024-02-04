1af4b97921940a4c1f4524906445ae34c7cdeeec
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2024-02-04 10:46:07 +0530

    Added emergency page and migrated existing emergency page to emergency-data

--- src/app/(protected)/emergency/page.js
+++ src/app/(protected)/emergency/page.js
@@ -1,26 +1,36 @@
 'use client';
-
-import { toast } from '@/components/ui/Toast';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
-import { db } from '@/config/firebase';
-import { collection, doc, getDoc, setDoc } from 'firebase/firestore/lite';
-import { useSession } from 'next-auth/react';
-
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
-import { BiLoaderAlt } from 'react-icons/bi';
+
+import { FaHospital } from 'react-icons/fa';
+import { FaPerson } from 'react-icons/fa6';
+import { MdEmergency } from 'react-icons/md';
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { toast } from '@/components/ui/Toast';
+import { useSession } from 'next-auth/react';
+import { doc, getDoc } from 'firebase/firestore/lite';
+import { db } from '@/config/firebase';
+import Greeting from '@/components/dashboard/Greeting';
 
 const Emergency = () => {
   const { data } = useSession();
+  const userFirstName = data?.user?.name ? data.user.name.split(" ")[0] : "";
 
   const [loading, setLoading] = useState(false);
 
@@ -38,195 +48,125 @@ const Emergency = () => {
 
   const loadEmergencyData = async (data) => {
     setLoading(true);
-    if(data?.user?.email){
-        const docRef = doc(db, 'emergencyData', data?.user?.email);
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-          setEmergencyData(docSnap.data());
-          setLoading(false);
-        }else{
-          setLoading(false);
-        }
+    if (data?.user?.email) {
+      const docRef = doc(db, 'emergencyData', data?.user?.email);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setEmergencyData(docSnap.data());
+        setLoading(false);
+      } else {
+        setLoading(false);
+      }
     }
   };
-  useEffect(() => { 
+  useEffect(() => {
     loadEmergencyData(data);
   }, [data]);
+  const menuItems = [
+    {
+      color: 'amber',
+      label: 'Family',
+      href: `tel:${emergencyData?.contect}`,
+      icon: FaPerson,
+    },
+    {
+      color: 'rose',
+      label: 'Emergency',
+      href: 'tel:7827170170',
+      icon: MdEmergency,
+    },
+    { color: 'green', label: 'Hospital', href: 'tel:108', icon: FaHospital },
+  ];
 
-  async function handleSubmit(e) {
-    e.preventDefault();
-    const docRef = collection(db, 'emergencyData');
-    setLoading(true);
-    setDoc(doc(docRef, data?.user?.email), { ...emergencyData })
-      .then(() => {
-        window.location.href = '/dashboard';
-      })
-      .catch((e) => {
-        toast.error('Something went wrong');
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+  const tableData = [
+    ['National Commission for Women Helpline', '7827170170'],
+    [
+      'Central Social Welfare Board -Police Helpline',
+      '1091/ 1291, (011) 23317004',
+    ],
+    ['Shakti Shalini', '10920'],
+    ["Shakti Shalini - women's shelter", '(011) 24373736/ 24373737'],
+    ['SAARTHAK', '(011) 26853846/ 26524061'],
+    ["All India Women's Conference", '10921/ (011) 23389680'],
+    ['JAGORI', '(011) 26692700 +918800996640'],
+    [
+      "Joint Women's Programme (also has branches in Bangalore, Kolkata, Chennai)",
+      '(011) 24619821',
+    ],
+    ['Sakshi - violence intervention center', '(0124) 2562336/ 5018873'],
+    ['Saheli - a womens organization', '(011) 24616485 (Saturdays)'],
+    ['Nirmal Niketan', '(011) 27859158'],
+    ['Nari Raksha Samiti', '(011) 23973949'],
+    [
+      'RAHI Recovering and Healing from Incest. A support centre for women survivors of child sexual abuse',
+      '(011) 26238466/ 26224042, 26227647',
+    ],
+  ];
+
+  function GridElement({ key, data }) {
+    return (
+      <Link href={data.href}>
+        <div
+          key={key}
+          className={`w-full aspect-square rounded-md flex flex-col justify-center items-center shadow-md gap-1 bg-gradient-to-br from-${data.color}-100 to-${data.color}-200 text-${data.color}-950`}
+        >
+          {React.createElement(data.icon, {
+            className: `w-1/2 h-1/3 fill-${data.color}-900`,
+          })}
+          <span className='font-semibold'>{data.label}</span>
+        </div>
+      </Link>
+    );
   }
   return (
     <div>
-      <h1 className='mb-2'>Your Emergency Data</h1>
-      <form
-        onSubmit={handleSubmit}
-        className='flex flex-col gap-4 accent-pink-400'
-      >
-        <div className='grid w-full items-center gap-1.5'>
-          <Label htmlFor='name'>Name</Label>
-          <Input
-            type='text'
-            id='name'
-            disabled={loading}
-            value={emergencyData.name}
-            onChange={(e) =>
-              setEmergencyData({ ...emergencyData, name: e.target.value })
-            }
-            className='focus:outline-none focus:ring-offset-0 focus-visible:ring-offset-0'
-          />
-        </div>
-        <div className='grid w-full items-center gap-1.5'>
-          <Label htmlFor='contact'>Contact</Label>
-          <Input
-            type='tel'
-            id='contact'
-            disabled={loading}
-            value={emergencyData.contact}
-            onChange={(e) =>
-              setEmergencyData({ ...emergencyData, contact: e.target.value })
-            }
-            className='focus:outline-none focus:ring-offset-0 focus-visible:ring-offset-0'
-          />
-        </div>
-        <div className='grid w-full items-center gap-1.5'>
-          <Label htmlFor='address'>Address</Label>
-          <Input
-            type='text'
-            id='address'
-            disabled={loading}
-            value={emergencyData.address}
-            onChange={(e) =>
-              setEmergencyData({ ...emergencyData, address: e.target.value })
-            }
-            className='focus:outline-none focus:ring-offset-0 focus-visible:ring-offset-0'
-          />
-        </div>
-        <div className='grid w-full items-center gap-1.5'>
-          <Label htmlFor='sex'>Sex</Label>
-          <Select
-            value={emergencyData.sex}
-            disabled={loading}
-            onValueChange={(value) => {
-              setEmergencyData({ ...emergencyData, sex: value });
-            }}
-            id='sex'
-          >
-            <SelectTrigger className='w-full'>
-              <SelectValue placeholder='Select a gender' />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value='male'>Male</SelectItem>
-                <SelectItem value='female'>Female</SelectItem>
-                <SelectItem value='not-set'>Not Set</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-        <div className='grid w-full items-center gap-1.5'>
-          <Label htmlFor='organ-donor'>Organ Donor</Label>
-          <Select
-            value={emergencyData.organDonor}
-            onValueChange={(value) => {
-              setEmergencyData({ ...emergencyData, organDonor: value });
-            }}
-            disabled={loading}
-            id='organ-donar'
-          >
-            <SelectTrigger className='w-full'>
-              <SelectValue placeholder='Select a gender' />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value='yes'>Yes</SelectItem>
-                <SelectItem value='no'>No</SelectItem>
-                <SelectItem value='not-set'>Not Set</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-        <div className='grid w-full items-center gap-1.5'>
-          <Label htmlFor='medical-coditions'>Medical Conditions</Label>
-          <Input
-            type='text'
-            disabled={loading}
-            value={emergencyData.medicalConditions}
-            onChange={(e) =>
-              setEmergencyData({
-                ...emergencyData,
-                medicalConditions: e.target.value,
-              })
-            }
-            id='medical-coditions'
-            className='focus:outline-none focus:ring-offset-0 focus-visible:ring-offset-0'
-          />
-        </div>
-        <div className='grid w-full items-center gap-1.5'>
-          <Label htmlFor='medications'>Medications</Label>
-          <Input
-            type='text'
-            disabled={loading}
-            value={emergencyData.medications}
-            onChange={(e) =>
-              setEmergencyData({
-                ...emergencyData,
-                medications: e.target.value,
-              })
-            }
-            id='medications'
-            className='focus:outline-none focus:ring-offset-0 focus-visible:ring-offset-0'
-          />
-        </div>
-        <div className='grid w-full items-center gap-1.5'>
-          <Label htmlFor='algAndRxn'>Allergies and Reactions</Label>
-          <Input
-            type='text'
-            disabled={loading}
-            value={emergencyData.allergiesAndReactions}
-            onChange={(e) =>
-              setEmergencyData({
-                ...emergencyData,
-                allergiesAndReactions: e.target.value,
-              })
-            }
-            id='algAndRxn'
-            className='focus:outline-none focus:ring-offset-0 focus-visible:ring-offset-0'
-          />
-        </div>
-        <div className='grid w-full items-center gap-1.5'>
-          <Label htmlFor='remarks'>Remarks</Label>
-          <Input
-            type='text'
-            disabled={loading}
-            value={emergencyData.remarks}
-            onChange={(e) =>
-              setEmergencyData({ ...emergencyData, remarks: e.target.value })
-            }
-            id='remarks'
-            className='focus:outline-none focus:ring-offset-0 focus-visible:ring-offset-0'
-          />
-        </div>
-        <button
-          type='submit'
-          disabled={loading}
-          className='bg-gradient-to-tr cursor-pointer from-pink-400 to-pink-300 text-white font-semibold p-2 text-center w-full flex flex-row justify-center items-center gap-2 rounded-md'
-        >
-          {loading && <BiLoaderAlt className='text-xl animate-spin' />} Save
-        </button>
-      </form>
+    <Greeting userFirstName={userFirstName} />
+      <h3 className='text-lg mb-2'>Emergency Contacts</h3>
+      <div className='grid grid-cols-3 gap-4'>
+        {menuItems.map((item) => (
+          <GridElement key={item.label} data={item} />
+        ))}
+      </div>
+
+      <Accordion className='my-4' type='single' collapsible>
+        <AccordionItem value='item-1'>
+          <AccordionTrigger className='bg-gray-50 text-black px-2 py-3 text-lg font-semibold'>
+            Additional Contact Options
+          </AccordionTrigger>
+          <AccordionContent>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Help Available on</TableHead>
+                  <TableHead className='text-right'>Contact</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+
+                {tableData.map((item, index) => (
+                  <TableRow key={index}>
+                    <TableCell>{item[0]}</TableCell>
+                    <TableCell
+                      className='text-right'
+                      onClick={() => {
+                        navigator.clipboard.writeText(item[1]);
+                        toast.success('Copied to clipboard');
+                      }}
+                    >
+                      {item[1]}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+
+      <Link href='/emergency-data' className='cursor-pointer bg-gray-50 text-black font-semibold px-2 py-3 text-center w-full flex flex-row justify-start items-center gap-2 rounded-md'>
+          View/Update Emergency Data
+     
+      </Link>
     </div>
   );
 };
