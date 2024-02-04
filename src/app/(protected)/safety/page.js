e51f197367dca6d07d59f2df0effe35a1e033102
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2024-02-04 22:10:58 +0530

    Add nearby resources and emergency page

--- src/app/(protected)/safety/page.js
+++ src/app/(protected)/safety/page.js
@@ -1,5 +1,12 @@
 'use client';
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
+import { BsHospital } from 'react-icons/bs';
+import { FaLocationArrow } from 'react-icons/fa';
+import { GrUserPolice } from 'react-icons/gr';
+import { IoIosArrowForward } from 'react-icons/io';
+import { MdEmergency } from 'react-icons/md';
+import { GoAlertFill } from 'react-icons/go';
 
 const Safety = () => {
   const [isLocationPermissionGranted, setIsLocationPermissionGranted] = useState(false);
@@ -32,10 +39,56 @@ const Safety = () => {
     }
   }, []);
 
+  const openNearby = (x) => {
+    window.open(
+      `https://www.google.com/maps/search/${x}/@${localStorage.getItem(
+        "sakhi_lat"
+      )},${localStorage.getItem("sakhi_long")}`,
+      "_blank"
+    );
+  }
+
   return (
     <div>
       {!isLocationPermissionGranted &&
         'Please allow location permission to use this feature.'}
+
+        <p className='my-2'>Here are some nearby resources to help you out</p>
+        <div className='border shadow-sm border-gray-200 rounded-xl'>
+        <button
+          onClick={() => openNearby("hospitals")}
+          className='cursor-pointer p-3 text-center w-full flex flex-row justify-between items-center gap-2 rounded-xl'
+        >
+          <div className='flex flex-row justify-start items-center gap-2 '>
+            <BsHospital className='text-lg text-rose-600' />
+            Hospitals
+          </div>
+          <FaLocationArrow className='text-lg' />
+        </button>
+        <hr />
+        <button
+          onClick={() => openNearby("police station")}
+          className='cursor-pointer p-3 text-center w-full flex flex-row justify-between items-center gap-2 rounded-xl'
+        >
+          <div className='flex flex-row justify-start items-center gap-2 '>
+            <GrUserPolice className='text-lg text-emerald-600' />
+            Police Station
+          </div>
+          <FaLocationArrow className='text-lg' />
+        </button>
+        
+        <hr />
+        <Link
+          href={'/emergency'}
+          className='cursor-pointer p-3 text-center w-full flex flex-row justify-between items-center gap-2 rounded-xl'
+        >
+          <div className='flex flex-row justify-start items-center gap-2 '>
+            <MdEmergency className='text-lg text-pink-600' />
+            Emergency Page
+          </div>
+          <IoIosArrowForward className='text-lg' />
+        </Link>
+       </div>
     </div>
   );
 };
