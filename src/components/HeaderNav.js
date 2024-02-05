cf57d4b962b916ba3c0d77b57d63fed72af27a41
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2024-02-05 09:40:43 +0530

    SOS make call feature

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -7,6 +7,7 @@ import { RiHeart2Line } from "react-icons/ri";
 import { MdOutlineSos } from "react-icons/md";
 import { IoIosWarning } from "react-icons/io";
 import { Button } from "./ui/button";
+import { toast } from "./ui/Toast";
 
 export default function HeaderNav() {
   return (
@@ -19,6 +20,7 @@ export default function HeaderNav() {
           <Button
             variant="destructive"
             className="flex gap-1 justify-center items-center py-1 px-4 text-3xl text-red-50 ring-2 ring-red-400 rounded-full bg-[#E04759]"
+            onClick={makeCall}
           >
             <IoIosWarning className="text-lg" />
             <MdOutlineSos className="" />
@@ -46,4 +48,24 @@ function UserComponent() {
       )}
     </>
   );
+}
+
+async function makeCall() {
+  try {
+    // Request permission to use telephony features
+    await navigator.permissions
+      .query({ name: "telephony" })
+      .then((permissionStatus) => {
+        if (permissionStatus.state === "granted") {
+          // If permission is granted, make the call
+          const phoneNumber = "100";
+          window.navigator.telephony.dial(phoneNumber);
+        } else {
+          // Permission not granted
+          toast.error("Telephony permission denied.");
+        }
+      });
+  } catch (error) {
+    console.error("Error making the call:", error);
+  }
 }
