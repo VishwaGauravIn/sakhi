d2fb9c16b6e35e772f78e32fe0539668b910a9d8
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2024-02-05 09:36:14 +0530

    Homepage redirect

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,5 @@
-"use client";
+import { redirect } from "next/navigation";
 
-import { toast } from "@/components/ui/Toast";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import React from "react";
-
-export default function Home() {
-  return (
-    <div>
-      <Button onClick={() => toast.success("Hello World! Click me for toast")}>
-        Hello World! Click me for toast
-      </Button>
-      <br />
-      <Link href="/login">Login</Link>
-      <br />
-      <Link href="/dashboard">Dashboard</Link>
-
-      <button onClick={() => getData()}>click</button>
-    </div>
-  );
+export default function HomeRedirect() {
+  redirect("/dashboard");
 }
